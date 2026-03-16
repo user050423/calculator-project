d2316c29fe8ff81db62f85ba1af6a255f4dcbca1
Ynotkokonut <naijabami@gmail.com>
2026-03-16 22:49:28 +0800

    basic math operations functions ( + , - , * , / )

---
+++ script.js
@@ -0,0 +1,24 @@
+/*Your calculator is going to contain functions for all of the basic math operators 
+you typically find on calculators, so start by creating functions for the following items
+and testing them in your browser’s console:
+add
+subtract
+multiply
+divide*/
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) { return 'cannot divide by zero'}
+    return a / b;
+}
