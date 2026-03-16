537986c6f470a695474fde69b3c5316eb63bb349
Ynotkokonut <naijabami@gmail.com>
2026-03-16 23:35:03 +0800

    array of buttons (trial and error)

--- script.js
+++ script.js
@@ -55,3 +55,9 @@ function operate(num1, operator, num2){
 }
 
 console.log(operate(10, 'add', 5));
+
+/*Create a basic HTML calculator with buttons for each digit and operator (including =).
+Don’t worry about making them functional just yet.
+There should also be a display for the calculator. Go ahead and fill it with some dummy numbers so it looks correct.
+Add a “clear” button.*/
+const buttons = ['AC', 'DEL', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '.', '0', '=']
