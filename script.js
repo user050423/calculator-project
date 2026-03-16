3a50f6409025dfa26f70513ba5d244b5932becc2
Ynotkokonut <naijabami@gmail.com>
2026-03-16 23:24:52 +0800

    create operate function
    receives 2 numbers and operator and does the operation

--- script.js
+++ script.js
@@ -29,3 +29,29 @@ You’ll use these variables to update your display later.*/
 let num1;
 let operator;
 let num2;
+
+/* Create a new function operate that takes an operator and two numbers 
+and then calls one of the above functions on the numbers.*/
+function operate(num1, operator, num2){
+    let result;
+
+    switch (operator) {
+        case 'add':
+            result = add(num1, num2);
+            break;
+        case 'subtract':
+            result = subtract(num1, num2);
+            break;
+        case 'multiply':
+            result = multiply(num1, num2);
+            break;
+        case 'divide':
+            result = divide(num1, num2);
+            break;
+        default:
+            result = "error";
+    }
+    return result;
+}
+
+console.log(operate(10, 'add', 5));
