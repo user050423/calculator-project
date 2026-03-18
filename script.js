d0f9005e512278fad51edd9018c26d0f16645917
Ynotkokonut <naijabami@gmail.com>
2026-03-18 20:01:53 +0800

    add eventlistener for digits button

--- script.js
+++ script.js
@@ -94,10 +94,18 @@ displayDiv.appendChild(showNumbers);
 const operatorButtons = document.querySelectorAll('.operators');
  operatorButtons.forEach(button => {
     button.addEventListener('click', (e) => {
-    num1 = e.target.textContent;
-    console.log(num1)
+    operator = e.target.textContent;
+    console.log(operator)
     });
 });
+
+const digitButtons = document.querySelectorAll('.digits');
+    digitButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            num1 = e.target.textContent;
+            console.log(num1)
+        })
+    })
 
 //create a while looop inside a function that checks if the button being pressed 
 //has a class of digits, store element with class digits in a variable
